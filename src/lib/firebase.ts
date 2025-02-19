95dbbc60044238ef2a36868a055941baa734405f
commonsense <kks3782@gmail.com>
2025-02-19 21:28:52 +0900

    태스크 테스트1

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,20 @@
 // lib/firebase.ts
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcl9hVJNKeC1BRzieT7r3BF5YyMK7hvb0",
-  authDomain: "ccgg-on-and-earn-cee78.firebaseapp.com",
-  projectId: "ccgg-on-and-earn-cee78",
-  storageBucket: "ccgg-on-and-earn-cee78.firebasestorage.app",
-  messagingSenderId: "296882338228",
-  appId: "1:296882338228:web:f98fb5b4fdb715d7021ae1",
-  measurementId: "G-JD989S7Y57"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
