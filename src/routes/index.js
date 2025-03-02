01ce3fef3b4a6e63c018a2e9f92c294b51f2dfd2
commonsense <kks3782@gmail.com>
2025-03-02 15:20:11 +0900

    다크모드

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,12 @@ import EventList from '../features/raffle/pages/EventList';
 import EventDetail from '../features/raffle/pages/EventDetail';
 import CreateEvent from '../features/raffle/pages/CreateEvent';
 
-const AppRoutes = () => {
+const AppRoutes = ({ telegramUser, isAdmin }) => {
   return (
     <Routes>
-      <Route path="/" element={<EventList />} />
-      <Route path="/event/:id" element={<EventDetail />} />
-      <Route path="/create" element={<CreateEvent />} />
+      <Route path="/" element={<EventList telegramUser={telegramUser} isAdmin={isAdmin} />} />
+      <Route path="/event/:id" element={<EventDetail telegramUser={telegramUser} isAdmin={isAdmin} />} />
+      <Route path="/create" element={<CreateEvent telegramUser={telegramUser} isAdmin={isAdmin} />} />
       {/* 미래에 추가될 라우트 */}
       <Route path="/tasks" element={<div>Tasks Page (벤)</div>} />
       <Route path="/earn" element={<div>Earn Page (타)</div>} />
